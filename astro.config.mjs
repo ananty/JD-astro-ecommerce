043c94aad14b8440005b1dc2b8699b3c886e1e5a
ananty <no.utopia.231@gmail.com>
2025-06-14 23:26:56 +0700

    all done
    all done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,19 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react"; // 如果你保留了 .tsx 组件，就需要这个
 import sitemap from "@astrojs/sitemap"; // 1. 导入 sitemap
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
+   output: 'server',
   integrations: [
     react(), 
     tailwind(), 
     sitemap() // 3. 添加 sitemap 集成
   ],
-   site: 'https://www.jaideepass.com', // 部署时再配置
+
+  // 部署时再配置
   // base: '/YOUR_REPOSITORY_NAME',       // 部署时再配置
+  site: 'https://www.jaideepass.com',
+
+  adapter: cloudflare()
 });
